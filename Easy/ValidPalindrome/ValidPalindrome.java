e807379f64228fba01ac4a529a562ec436a416f5
Braden Ash <ashbraden1@hotmail.com>
2021-06-24 11:40:24 -0700

    Optimized Solution
    Remove need for translating string to only lowercase characters. Use Character function to determine alphanumeric as opposed to create new string.

--- Easy/ValidPalindrome/ValidPalindrome.java
+++ Easy/ValidPalindrome/ValidPalindrome.java
@@ -1,19 +1,40 @@
 public class ValidPalindrome {
+    // public boolean isPalindrome(String s) {
+    // String condensed = "";
+    // s = s.toLowerCase();
+
+    // for (int i = 0; i < s.length(); i++) {
+    // int index = (int) s.charAt(i);
+    // if ((index >= 48 && index <= 57) || (index >= 97 && index <= 122)) {
+    // condensed += s.charAt(i);
+    // }
+    // }
+
+    // for (int i = 0; i < condensed.length() / 2; i++) {
+    // int end = condensed.length() - 1 - i;
+    // if (condensed.charAt(i) != condensed.charAt(end)) {
+    // return false;
+    // }
+    // }
+    // return true;
+    // }
+
     public boolean isPalindrome(String s) {
-        String condensed = "";
-        s = s.toLowerCase();
-
-        for (int i = 0; i < s.length(); i++) {
-            int index = (int) s.charAt(i);
-            if ((index >= 48 && index <= 57) || (index >= 97 && index <= 122)) {
-                condensed += s.charAt(i);
-            }
-        }
-
-        for (int i = 0; i < condensed.length() / 2; i++) {
-            int end = condensed.length() - 1 - i;
-            if (condensed.charAt(i) != condensed.charAt(end)) {
-                return false;
+        int end = s.length() - 1;
+        int start = 0;
+        while (start < end) {
+            char first = s.charAt(start);
+            char last = s.charAt(end);
+            if (!Character.isLetterOrDigit(first)) {
+                start++;
+            } else if (!Character.isLetterOrDigit(last)) {
+                end--;
+            } else {
+                if (start < end && Character.toLowerCase(first) != Character.toLowerCase(last)) {
+                    return false;
+                }
+                start++;
+                end--;
             }
         }
         return true;
